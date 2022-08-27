8cd22da5202c14915ed91ba5349ea3b927bdc704
Yamil Tactuk <yamil.tactuk@gmail.com>
2022-08-27 18:56:06 -0400

    Completed the email addition workflow

--- pupeteer/app.js
+++ pupeteer/app.js
@@ -8,32 +8,52 @@ const config = {
   GMAIL_PASSWORD_B64: process.env.GMAIL_PASSWORD_B64 || undefined
 };
 
+//TODO: get new member email
+const newMemberEmail = "new-member@gmail.com";
+
 (async () => {
 
     const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
     const pages = await browser.pages();
     const page = pages[0];
     await page.goto('https://groups.google.com/g/domination-study-group/members');
-    const textBox = await page.$('#identifierId');
-    await textBox.type(config.GMAIL_EMAIL);
-    await textBox.press('Enter');
+    const emailBox = await page.$('#identifierId');
+    await emailBox.type(config.GMAIL_EMAIL);
+    await emailBox.press('Enter');
 
     const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
-    await snooze(5000);
+    await snooze(1000);
 
     const passwordBox = await page.$("input[type='password']");
     if(passwordBox != null)
     {
       let buff = new Buffer.from(config.GMAIL_PASSWORD_B64, 'base64');
-      let pass = buff.toString('ascii').replace('/\r\n//');
+      let pass = buff.toString('ascii');
       await passwordBox.type(pass); // Somehow this is sending new line
+      await passwordBox.press('Enter');
     } else {
       console.error("couldn't find the password box")
     }
 
-    //   await page.screenshot({path: 'example-matey.png'});
     await snooze(5000);
 
+    
+    await page.click("div[aria-label='Add members']");
+
+    await snooze(1000);
+    
+    const addGroupMembers = await page.$("input[aria-label='Group members']");
+    await addGroupMembers.type(newMemberEmail);
+    const invitationMessage = await page.$("textarea[aria-label='Invitation message']");
+    await invitationMessage.type("Welcome to Domination group!");
+
+    await snooze(1000);
+
+    //await page.click("div[aria-label='Send invites']");
+
+    
+    await snooze(5000);
+    
 
     //await browser.close();
 })();
