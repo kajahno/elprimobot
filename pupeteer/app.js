f60ac9b04075c784fa06e23bc2df340a220953af
Yamil Tactuk <yamil.tactuk@gmail.com>
2022-08-27 16:45:28 -0400

    added .env implemenations and if the password textbox is not in it's place, don't explote

--- pupeteer/app.js
+++ pupeteer/app.js
@@ -1,15 +1,23 @@
 const puppeteer = require('puppeteer');
 
+const config = {
+  GMAIL_EMAIL: process.env.GMAIL_EMAIL || undefined,
+  GGMAIL_PASSWORD: process.env.GGMAIL_PASSWORD || undefined
+};
+
 (async () => {
     const browser = await puppeteer.launch({ headless: false, slowMo: 100 });
     const page = await browser.newPage();
     await page.goto('https://groups.google.com/g/domination-study-group/members');
     const textBox = await page.$('#identifierId');
-    await textBox.type('dummy-email@gmail.com');
+    await textBox.type(config.GMAIL_EMAIL);
     await textBox.press('Enter');
-    const passwordBox = await page.$('dummy-password');
-    await passwordBox.type('');
-    await passwordBox.press('Enter');
+    const passwordBox = await page.$('[name=password]');
+    if(passwordBox != null)
+    {
+      await passwordBox.type(config.GGMAIL_PASSWORD);
+      await passwordBox.press('Enter');
+    }
 
     //   await page.screenshot({path: 'example-matey.png'});
 
@@ -23,5 +31,5 @@ const puppeteer = require('puppeteer');
     await example();
 
 
-    await browser.close();
+    //await browser.close();
 })();
