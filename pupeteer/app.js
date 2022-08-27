82df05cfced9b3e925d74f7d65e181210c152ce2
Karl Jahn <kajahno@gmail.com>
2022-08-27 18:58:02 -0300

    Finish authentication

--- pupeteer/app.js
+++ pupeteer/app.js
@@ -1,34 +1,38 @@
-const puppeteer = require('puppeteer');
+require("dotenv").config();
+const puppeteer = require('puppeteer-extra')
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
 
 const config = {
   GMAIL_EMAIL: process.env.GMAIL_EMAIL || undefined,
-  GGMAIL_PASSWORD: process.env.GGMAIL_PASSWORD || undefined
+  GMAIL_PASSWORD_B64: process.env.GMAIL_PASSWORD_B64 || undefined
 };
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: false, slowMo: 100 });
-    const page = await browser.newPage();
+
+    const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
+    const pages = await browser.pages();
+    const page = pages[0];
     await page.goto('https://groups.google.com/g/domination-study-group/members');
     const textBox = await page.$('#identifierId');
     await textBox.type(config.GMAIL_EMAIL);
     await textBox.press('Enter');
-    const passwordBox = await page.$('[name=password]');
+
+    const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
+    await snooze(5000);
+
+    const passwordBox = await page.$("input[type='password']");
     if(passwordBox != null)
     {
-      await passwordBox.type(config.GGMAIL_PASSWORD);
-      await passwordBox.press('Enter');
+      let buff = new Buffer.from(config.GMAIL_PASSWORD_B64, 'base64');
+      let pass = buff.toString('ascii').replace('/\r\n//');
+      await passwordBox.type(pass); // Somehow this is sending new line
+    } else {
+      console.error("couldn't find the password box")
     }
 
     //   await page.screenshot({path: 'example-matey.png'});
-
-
-    const snooze = ms => new Promise(resolve => setTimeout(resolve, ms));
-    const example = async () => {
-      console.log('About to snooze without halting the event loop...');
-      await snooze(5000);
-      console.log('done!');
-    };
-    await example();
+    await snooze(5000);
 
 
     //await browser.close();
