9bc361af835165d1bbdf083bd2cb9ffe7fdd217c
Karl Jahn <kajahno@gmail.com>
2022-10-02 18:59:33 +0100

    Enable interactions and: * Refactor to use es6 syntax * Add all commands to build and package in package.json * Leave default interactions from discordjs example * Configure babel and update documentation * add commands as cron * refactor previous leetcodedailymessage command

---
+++ commands.js
@@ -0,0 +1,82 @@
+import { getRPSChoices } from './game.js';
+import { capitalize, DiscordRequest } from './utils.js';
+
+export async function HasGuildCommands(appId, guildId, commands) {
+  if (guildId === '' || appId === '') return;
+
+  commands.forEach((c) => HasGuildCommand(appId, guildId, c));
+}
+
+// Checks for a command
+async function HasGuildCommand(appId, guildId, command) {
+  // API endpoint to get and post guild commands
+  const endpoint = `applications/${appId}/guilds/${guildId}/commands`;
+
+  try {
+    const res = await DiscordRequest(endpoint, { method: 'GET' });
+    const data = await res.json();
+
+    if (data) {
+      const installedNames = data.map((c) => c['name']);
+      // This is just matching on the name, so it's not good for updates
+      if (!installedNames.includes(command['name'])) {
+        console.log(`Installing "${command['name']}"`);
+        InstallGuildCommand(appId, guildId, command);
+      } else {
+        console.log(`"${command['name']}" command already installed`);
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// Installs a command
+export async function InstallGuildCommand(appId, guildId, command) {
+  // API endpoint to get and post guild commands
+  const endpoint = `applications/${appId}/guilds/${guildId}/commands`;
+  // install command
+  try {
+    await DiscordRequest(endpoint, { method: 'POST', body: command });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// Get the game choices from game.js
+function createCommandChoices() {
+  const choices = getRPSChoices();
+  const commandChoices = [];
+
+  for (let choice of choices) {
+    commandChoices.push({
+      name: capitalize(choice),
+      value: choice.toLowerCase(),
+    });
+  }
+
+  return commandChoices;
+}
+
+// Simple test command
+export const TEST_COMMAND = {
+  name: 'test',
+  description: 'Basic guild command',
+  type: 1,
+};
+
+// Command containing options
+export const CHALLENGE_COMMAND = {
+  name: 'challenge',
+  description: 'Challenge to a match of rock paper scissors',
+  options: [
+    {
+      type: 3,
+      name: 'object',
+      description: 'Pick your object',
+      required: true,
+      choices: createCommandChoices(),
+    },
+  ],
+  type: 1,
+};
